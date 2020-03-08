89f6e101b4e507e6e61926d85ab21310b69a6540
Fernando Athaide <fernandoathaide@hotmail.com>
2020-03-08 11:31:57 -0300

    Teste Unitários metods USER ok Finalizados.

--- backend_sistad/build/tests/unit/unit.test.js
+++ backend_sistad/build/tests/unit/unit.test.js
@@ -4,26 +4,32 @@ var helpers_1 = require("./config/helpers");
 var serviceUser_1 = require("../../server/modules/User/serviceUser");
 describe('Testes Unitários do userController', function () {
     describe('Método Create', function () {
+        //CRIAR USER TESTE
         it('Deve Criar um novo Usuário', function () {
             var user = {
-                id: 1,
+                id_user: 1,
                 name: 'Teste Unitário',
                 email: 'teste@unitario.com',
                 password: 'teste'
             };
             return serviceUser_1.default.create(user)
                 .then(function (data) {
+                console.log('DATA CREATE data.dataValues == ');
+                console.log(JSON.stringify(data.dataValues));
                 helpers_1.expect(data.dataValues).to.have.all.keys(['email', 'id_user', 'name', 'password', 'updatedAt', 'createdAt']);
             });
         });
     });
-    /*
-        describe('Método Update', () => {
-            it('Deve Alterar um Usuário', () =>{
-    
+    //UPDATE USER TESTE
+    describe('Método Update', function () {
+        it('Deve Alterar um Usuário', function () {
+            var userUpdate = { name: 'UserUpdate', email: 'userUpdate@teste.com' };
+            return serviceUser_1.default.update(1, userUpdate).then(function (data) {
+                helpers_1.expect(data[0]).to.be.equal(1);
             });
         });
-    */
+    });
+    //BUSCAR TODOS USER TESTE
     describe('Método Buscar Usuários', function () {
         it('Deve Retornar Lista com os Usuários', function () {
             return serviceUser_1.default.getAll().then(function (data) {
@@ -32,11 +38,13 @@ describe('Testes Unitários do userController', function () {
             });
         });
     });
-    /*
-        describe('Método Delete', () => {
-            it('Deve Deletar um Usuário', () =>{
-    
+    //EXCLUIR USER TESTE
+    describe('Método Delete', function () {
+        it('Deve Deletar um Usuário', function () {
+            return serviceUser_1.default.delete(1).then(function (data) {
+                //expect(data[0]).to.be.equal(1);
+                console.log('DATA NA TELA = ' + data[0]);
             });
         });
-    */
+    });
 });
--- backend_sistad/tests/unit/unit.test.ts
+++ backend_sistad/tests/unit/unit.test.ts
@@ -1,30 +1,37 @@
 import { testDouble, expect } from './config/helpers';
 import ServiceUser from '../../server/modules/User/serviceUser';
+import { json } from 'body-parser';
 
 describe('Testes Unitários do userController', () =>{
     describe('Método Create', () => {
+        //CRIAR USER TESTE
         it('Deve Criar um novo Usuário', () =>{
             const user = { 
-                id: 1, 
+                id_user: 1, 
                 name: 'Teste Unitário', 
                 email: 'teste@unitario.com', 
                 password: 'teste' 
             };
             return ServiceUser.create(user)
             .then(data =>{
+                console.log('DATA CREATE data.dataValues == ');
+                console.log(JSON.stringify(data.dataValues));
                 expect(data.dataValues).to.have.all.keys(
                     ['email','id_user','name','password','updatedAt','createdAt']
                 )
             });
         });
     });
-/*
+    //UPDATE USER TESTE
     describe('Método Update', () => {
         it('Deve Alterar um Usuário', () =>{
-
+            const userUpdate = { name:'UserUpdate', email: 'userUpdate@teste.com'}
+            return ServiceUser.update(1, userUpdate).then(data => {
+                expect(data[0]).to.be.equal(1);
+            })
         });
     });
-*/
+    //BUSCAR TODOS USER TESTE
     describe('Método Buscar Usuários', () => {
         it('Deve Retornar Lista com os Usuários', () =>{
             return ServiceUser.getAll().then(data => {
@@ -35,11 +42,13 @@ describe('Testes Unitários do userController', () =>{
             })
         });
     });
-/*
+    //EXCLUIR USER TESTE
     describe('Método Delete', () => {
         it('Deve Deletar um Usuário', () =>{
-
+            return ServiceUser.delete(1).then(data => {
+                //expect(data[0]).to.be.equal(1);
+                console.log('DATA NA TELA = ' + data[0]);
+            })
         });
     });
-*/
 });
