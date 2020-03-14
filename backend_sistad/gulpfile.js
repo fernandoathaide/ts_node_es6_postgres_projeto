93ca2a139ff849aabed3c8d66cc832cfcf503d3f
Fernando Athaide <fernandoathaide@hotmail.com>
2020-03-14 00:21:48 -0300

    Configuração final integração continua.

--- backend_sistad/gulpfile.js
+++ backend_sistad/gulpfile.js
@@ -20,6 +20,13 @@ gulp.task('copyAllFiles', function() {//Lembrar de corrigir caminho relativo do 
   gulp.src('server/seenders/*').pipe(gulp.dest('dist/server/seenders'));
   gulp.src('server/config/config.json').pipe(gulp.dest('dist/server/config'));
   gulp.src('server/migrations/*').pipe(gulp.dest('dist/server/migrations'));
+
+  gulp.src(['server/api/swagger.json']).pipe(gulp.dest('build/server/api/'));
+  gulp.src('server/seenders/*').pipe(gulp.dest('build/server/seenders'));
+  gulp.src('server/config/config.json').pipe(gulp.dest('build/server/config'));
+  gulp.src('server/migrations/*').pipe(gulp.dest('build/server/migrations'));
+  gulp.src('tests/unit/config/mocha.opts').pipe(gulp.dest('build/tests/unit/config')).pipe(gulp.dest('build/tests/integration/config'));
+
   return gulp.src('tests/unit/config/mocha.opts').pipe(gulp.dest('dist/tests/unit/config')).pipe(gulp.dest('dist/tests/integration/config'));
 });
 
--- backend_sistad/server/config/env/teste.env.ts
+++ backend_sistad/server/config/env/teste.env.ts
@@ -1,12 +1,12 @@
 module.exports = {
     env: 'teste',
-    db: 'ts-api-teste',
+    db: 'ts_api_test',
     dialect: 'postgres',
     username: 'postgres',
     password: 'wff@260981N',
     host: 'localhost',
     serverPort: 3000,
     pgPort: 5432,
-    dbURL: 'postgres://postgres:wff%40260981N@localhost:5432/ts-api-teste',
+    dbURL: 'postgres://postgres:wff%40260981N@localhost:5432/ts_api_test',
     secret: 'teste'
 }
